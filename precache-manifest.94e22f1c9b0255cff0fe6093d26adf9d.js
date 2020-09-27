ae22ca9640189d3d8c9a513dbcc50b780d6a2fd4
Nikhil Choudhary <nc1336282@gmail.com>
2020-09-27 18:05:21 +0530

    Updates

---
+++ precache-manifest.94e22f1c9b0255cff0fe6093d26adf9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3715c49504537a9d43dd59c29ec990df",
+    "url": "/fabric-studio/index.html"
+  },
+  {
+    "revision": "9912aca843bd216c5511",
+    "url": "/fabric-studio/static/css/main.be601951.chunk.css"
+  },
+  {
+    "revision": "68f3b75ac46cea6adaf8",
+    "url": "/fabric-studio/static/js/2.96037aab.chunk.js"
+  },
+  {
+    "revision": "74b31feb45d57052916c9457b367874c",
+    "url": "/fabric-studio/static/js/2.96037aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9912aca843bd216c5511",
+    "url": "/fabric-studio/static/js/main.6292df16.chunk.js"
+  },
+  {
+    "revision": "d8c556b6726939b672b1",
+    "url": "/fabric-studio/static/js/runtime-main.fc3aad2b.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/fabric-studio/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/fabric-studio/static/media/shopping-bag.30f07071.svg"
+  }
+]);
